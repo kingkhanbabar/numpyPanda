944f41a8646d7352a7e620e6be5f1bacc7b0f689
kingkhanbabar <shahvejkhanjmi@gmail.com>
2026-01-24 12:33:40 +0530

    Initial commit: numpy and pandas practice

---
+++ 1.py
@@ -0,0 +1,8 @@
+p = [1,2,4,7,6,7,8,8]
+print(p)
+import numpy as np
+n = np.array([1,3,5,7,8,9])
+print(n)
+#2d array
+a2d = np.array([[1,2,3,4,5],[2,4,9,56,5],[9,8,7,6,8]])
+print(a2d)
---
+++ nparray/2dArray.py
@@ -0,0 +1,15 @@
+import numpy as np
+a1d = np.array([1,2,3,4,5])
+a2d =np.array([[1,2,34,5,8],[6,7,5,4,3]])
+a3d = np.array([[1,23,4],[4,7,8],[76,66,2]])
+a4d = np.array([[1,2,3,4],[5.9999,6,7,6],[6,6,5,4.76],[55,66,55,8.9]])
+print(a1d) 
+print(a1d.dtype)
+print(a2d)
+print(a3d)
+print('the value of array is  ',a3d)
+print("the type of 4d array is  ")
+print(a4d.dtype)
+print(a3d.dtype)
+
+
