de0817e9e7a42954eb6ffed3e30aac33bdda852e
kingkhanbabar <shahvejkhanjmi@gmail.com>
2026-01-29 16:37:38 +0530

    DistriButive Commit

--- 1.py
+++ 1.py
@@ -1,8 +1,34 @@
-p = [1,2,4,7,6,7,8,8]
-print(p)
-import numpy as np
-n = np.array([1,3,5,7,8,9])
-print(n)
-#2d array
-a2d = np.array([[1,2,3,4,5],[2,4,9,56,5],[9,8,7,6,8]])
-print(a2d)
+# p = [1,2,4,7,6,7,8,8]
+# print(p)
+# import numpy as np
+# n = np.array([1,3,5,7,8,9])
+# print(n)
+# #2d array
+# a2d = np.array([[1,2,3,4,5],[2,4,9,56,5],[9,8,7,6,8]])
+# print(a2d)
+
+def calc_sum(a,b):
+    s=a+b
+    print(s)
+    return s
+
+calc_sum(5,8)
+
+calc_sum(56,89)
+
+a = float(input("Enter a "))
+b = float(input("Enter b "))
+calc_sum(a,b)
+
+
+
+
+
+name = input("Enter your name")
+print(name)
+
+age = int(input("Enter your age"))
+print(age)
+
+course = input("Enter course")
+print(course)
